aec2dbf77fa8a17a675c81d9a01d4237f4dce867
Nigel Orion <motiusclyde@Nigels-MacBook-Pro.local>
2017-01-30 16:48:18 -0800

    resolve conflicts

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,15 +1,60 @@
 import { Component } from '@angular/core';
-
+import { Camera, CameraOptions } from 'ionic-native';
 import { NavController } from 'ionic-angular';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import firebase from 'firebase';
+
 
 @Component({
   selector: 'page-contact',
   templateUrl: 'contact.html'
 })
 export class ContactPage {
+  public currentImage: string;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public af: AngularFire) {
 
   }
+
+
+  takePicture(){
+
+    Camera.getPicture({
+        destinationType: Camera.DestinationType.DATA_URL,
+        targetWidth: 1000,
+        targetHeight: 1000
+    }).then((imageData) => {
+        this.currentImage = "data:image/jpeg;base64," + imageData;
+    }, (err) => {
+        console.log(err);
+    });
+  }
+
+
+  upload() {
+
+    let storageRef = firebase.storage().ref();
+
+    const filename = Math.floor(Date.now() / 1000);
+
+    const imageRef = storageRef.child(`images/${filename}.jpg`);
+
+    imageRef.putString(this.currentImage, firebase.storage.StringFormat.DATA_URL).then((snapshot)=> {
+      this.successAlert();
+    });
+
+
+    imageRef.getDownloadURL().then(function(url) {
+      alert(url);
+
+    });
+
+  }
+
+  successAlert() {
+    alert("Upload Successful!");
+  }
+
+
 
 }
